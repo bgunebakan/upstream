52b1f3ebc5fac2a7ba58dac321ca7f94a9723c63
bgunebakan <bilaltonga@gmail.com>
2017-12-05 23:30:13 +0300

    Remove duplicated directory

--- lib/python2.7/site-packages/djrichtextfield/urls.py
+++
@@ -1,9 +0,0 @@
-from __future__ import unicode_literals
-
-from django.conf.urls import url
-
-from djrichtextfield.views import InitView
-
-urlpatterns = [
-    url('^init.js$', InitView.as_view(), name='djrichtextfield_init')
-]
