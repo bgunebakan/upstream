f527e2cfd4d17410e9954bcf83bc1afc84a66881
Bilal Tonga <bilaltonga@gmail.com>
2018-05-10 23:34:32 +0300

    quick log fix

--- upstream/portunes/views.py
+++ upstream/portunes/views.py
@@ -166,8 +166,8 @@ def logs(request):
                 action_type = Action_type.objects.get(action_type=action_no)
                 try:
                     user = User.objects.get(id=identifier.user.id)
-                except KeyError:
-                    pass
+                except AttributeError:
+                    print "----------USER HAS DELETED--------"
             elif (action_no is 1) and (door.enter is not True): # card exit
                 action_type = Action_type.objects.get(action_type=2)
                 user = User.objects.get(id=identifier.user.id)
