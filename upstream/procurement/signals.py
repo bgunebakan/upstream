18d52a59dd4e7650fb234fabaefa1065224e6c79
bgunebakan <bilaltonga@gmail.com>
2019-11-27 12:16:21 +0300

    tender no create fix

--- upstream/procurement/signals.py
+++ upstream/procurement/signals.py
@@ -1,25 +1,27 @@
-from django.contrib.auth.models import User,Permission
-from django.db.models.signals import post_save,post_delete,pre_save
+from django.contrib.auth.models import User, Permission
+from django.db.models.signals import post_save, post_delete, pre_save
 from django.dispatch import receiver
 from filer.models import Folder
-from .models import Tender,TenderType
+from .models import Tender, TenderType
 from django.utils import timezone
 import datetime
 from constance import config
 
+
 @receiver(post_delete, sender=Tender)
 def count_item(sender, instance, **kwargs):
-    #print "counting"
+    # print "counting"
     tender_types = TenderType.objects.all()
 
     for tender_type in tender_types:
         tenders = Tender.objects.filter(tender_type=tender_type)
         tender_type.total = tenders.count()
         tender_type.save()
+
 
 @receiver(post_save, sender=Tender)
 def count_item(sender, instance, **kwargs):
-    #print "counting"
+    # print "counting"
     tender_types = TenderType.objects.all()
 
     for tender_type in tender_types:
@@ -27,9 +29,9 @@ def count_item(sender, instance, **kwargs):
         tender_type.total = tenders.count()
         tender_type.save()
 
+
 @receiver(pre_save, sender=Tender)
 def last_tender_no(sender, instance, **kwargs):
-
     if not instance.no:
         print "Generate new tender no"
         tenders = Tender.objects.filter(tender_type=instance.tender_type)
@@ -38,20 +40,23 @@ def last_tender_no(sender, instance, **kwargs):
         for tender in tenders:
             if tender.no:
                 print tender.no[6:10]
-                if unicode(datetime.datetime.now().year) in tender.no:
+                if tender.no[len(config.tender_no_code + 1):len(config.tender_no_code + 5)] == unicode(
+                        datetime.datetime.now().year):
+                    # if unicode(datetime.datetime.now().year) in tender.no:
                     tender_no_list.append(int(tender.no[-3:]))
         if tender_no_list:
             last_tender_no = max(tender_no_list) + 1
         else:
             last_tender_no = 1
 
-        tender_no = unicode(config.tender_no_code) +"-"+ unicode(datetime.datetime.now().year) + unicode(instance.tender_type.code)+unicode(last_tender_no).zfill(3)
+        tender_no = unicode(config.tender_no_code) + "-" + unicode(datetime.datetime.now().year) + unicode(
+            instance.tender_type.code) + unicode(last_tender_no).zfill(3)
         print tender_no
         instance.no = tender_no
         instance.save()
     else:
         print "Tender no will not generated"
 
-        #tender_type = TenderType.objects.get(id=instance.tender_type.id)
-        #tender_type.last_tender_no = tender_type.last_tender_no + 1
-        #tender_type.save()
+        # tender_type = TenderType.objects.get(id=instance.tender_type.id)
+        # tender_type.last_tender_no = tender_type.last_tender_no + 1
+        # tender_type.save()
