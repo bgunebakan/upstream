ad6e03f104dbe0254aac9ca06aced0819f179dd8
bgunebakan <bilaltonga@gmail.com>
2019-12-31 10:16:34 +0300

    actions on admin list fields

--- upstream/portunes/admin.py
+++ upstream/portunes/admin.py
@@ -8,7 +8,6 @@ from import_export import resources
 
 admin.site.register(Controller)
 admin.site.register(Identifier)
-admin.site.register(Action)
 admin.site.register(Action_type)
 admin.site.register(Door)
 admin.site.register(DoorGroup)
