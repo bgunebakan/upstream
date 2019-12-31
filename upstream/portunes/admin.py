fc7238ebaffda8564d529f51d98c2695f15fede6
bgunebakan <bilaltonga@gmail.com>
2019-12-31 10:14:58 +0300

    actions on admin list fields

--- upstream/portunes/admin.py
+++ upstream/portunes/admin.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 from .models import *
+from import_export.admin import ImportExportModelAdmin
+from import_export import resources
 
 admin.site.register(Controller)
 admin.site.register(Identifier)
@@ -11,3 +13,12 @@ admin.site.register(Action_type)
 admin.site.register(Door)
 admin.site.register(DoorGroup)
 admin.site.register(Permission)
+
+class ActionResource(resources.ModelResource):
+    class Meta:
+        model = Action
+        fields = ('user', 'identifier', 'door','action_type','created_date')
+
+@admin.register(Action)
+class ItemImportExportAdmin(ImportExportModelAdmin):
+    resource_class = ActionResource
